d2c27e7f5d029fb3fd43511e9e747df2bbc5c782
aqylay <98256557+aqylay@users.noreply.github.com>
2022-03-14 06:49:33 +0000

    Update setup.php

--- setup.php
+++ setup.php
@@ -1,8 +1,8 @@
 <?php
-$host = 'localhost'; // Your Host Name
-$dbuser = 'dbuser'; // Database Username
-$dbpass = 'dbpass'; // Database password
-$dbname = 'dbname'; // Database Name
-$siteurl = 'Your Website Url Without any Slash(/) in the End'; // Enter your website url without any slash(/) in the end
-$sitename = 'URL SHORTNER NAME'; // Your Website Name in 8 or less words
+$host = 'localhost'; //
+$dbuser = 'dbuser'; //
+$dbpass = 'dbpass'; //
+$dbname = 'dbname'; //
+$siteurl = 'ilol.ml'; //
+$sitename = 'DiscordUrlShortner'; //
 ?>
