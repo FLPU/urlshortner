b920b904657221784897f32fea2e732d46f4a0c9
aqylay <98256557+aqylay@users.noreply.github.com>
2022-03-14 06:50:49 +0000

    Update data.sql

--- data.sql
+++ data.sql
@@ -10,7 +10,4 @@ id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 url TEXT(300) NOT NULL,
 title VARCHAR(8)
 );
-INSERT INTO links (`url`, `title`)
-    VALUES ('https://www.facebook.com/iamhrwndr', 'hrwndr');
-    
-/* Import this file to your database created for url shortner. */
+
